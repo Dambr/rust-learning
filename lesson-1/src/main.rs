5a8fb131eb241a53854ed69f81371bd75a8d0f42
Dambr <dambr@localhost>
2026-01-08 20:18:54 +0300

    Выполнен урок №1

---
+++ lesson-1/src/main.rs
@@ -0,0 +1,21 @@
+/**
+ * Multiple line comment
+ */
+fn main() {
+    // Single lime comment
+
+    let num: u128 = 0;
+    println!("Result: {}", num);
+
+    let num: i128 = 1;
+    println!("Result: {}", num);
+
+    let num: f32 = 2.5;
+    println!("Result: {}", num);
+    
+    let num: bool = false;
+    println!("Result: {}", num);
+
+    let num: char = 'h';
+    println!("Result: {}", num);
+}
